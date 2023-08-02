46199b2f266a2e43d4ad61b58004ff2e2f41c439
Gourav Sathe <76203349+GouravSathe@users.noreply.github.com>
2023-08-02 17:07:16 +0530

    Update stripe.js

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -8,7 +8,7 @@ const stripe = Stripe(Y
 export const bookTour = async (tourId) => {
   try {
     // 1) Get checkout session from API
-    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    const session = await axios.get(`/api/v1/bookings/checkout-session/${tourId}`);
 
     // 2) Create checkout form + charge credit card
     const checkoutPageUrl = session.data.session.url;
